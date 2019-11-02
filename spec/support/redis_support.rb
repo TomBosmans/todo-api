acc04eaf00cc8947fc70d4cc1ad1fa20b0b9521e
Tom Bosmans <tom.94.bosmans@gmail.com>
2019-11-02 14:28:18 +0100

    Setup redis.

---
+++ spec/support/redis_support.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module RedisHelper
+  extend ActiveSupport::Concern
+
+  included do
+    after { Redis.current.flushdb }
+  end
+end
+
+RSpec.configure do |config|
+  config.include RedisHelper
+  config.before(:suite) { Redis.current.select(1) } # select db 1
+end
